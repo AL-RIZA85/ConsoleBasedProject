54c1e6deaf71add8b271ab9aff67fb6f21aa80d1
alriz <alriz@ALRIZA>
2025-09-11 17:03:00 +0700

    Add project files.

---
+++ ConsoleBasedProject/dataUmur.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBasedProject
+{
+    internal class dataUmur
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your username:");
+            string nama = Console.ReadLine();
+            Console.WriteLine("Please enter your age:");
+            int umur = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Halo " +nama+"umur anda"+umur);
+            Console.ReadKey();
+
+
+        }
+    }
+}
